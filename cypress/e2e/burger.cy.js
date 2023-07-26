d46e1f85570a3f602c76f0d05510ff80917e995a
fvorontsov <vorontsovfa@gmail.com>
2023-07-26 20:52:55 +0300

    naming

--- cypress/e2e/burger.cy.js
+++ cypress/e2e/burger.cy.js
@@ -1,105 +1,104 @@
-import {INGREDIENTS_URL, LOGIN_URL, ORDER_URL} from "./test-constants";
+import { INGREDIENTS_URL, LOGIN_URL, ORDER_URL } from "./constants";
 
-describe('service is available', () => {
-    beforeEach(() => {
-        cy.visit('/')
-        cy.get('[data-testid=ingredients-list').as('ingredientList')
-    })
+describe("service is available", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("[data-testid=ingredients-list").as("ingredientList");
+  });
 
-    it('should be available', () => {
-        cy.get('@ingredientList').should('exist')
-    })
+  it("Availability", () => {
+    cy.get("@ingredientList").should("exist");
+  });
 
-    it('should has ingredients', () => {
-        cy.get('@ingredientList').should('have.length.at.least', 2)
-    })
-})
+  it("Ingredients", () => {
+    cy.get("@ingredientList").should("have.length.at.least", 2);
+  });
+});
 
-describe('ingredient modal works', () => {
-    beforeEach(() => {
-        cy.visit('/')
-        cy.get("[data-testid=ingredients-list").as("ingredientList");
+describe("ingredient modal", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("[data-testid=ingredients-list").as("ingredientList");
 
-        cy.get('@ingredientList').eq(0).as('testIngredient');
-        cy.get("@testIngredient").eq(0).children('a').eq(0).as('testInner');
+    cy.get("@ingredientList").eq(0).as("testIngredient");
+    cy.get("@testIngredient").eq(0).children("a").eq(0).as("testInner");
 
-        cy.get("@testInner").click();
-    })
+    cy.get("@testInner").click();
+  });
 
-    it('should be', () => {
-        cy.get('[data-testid=modal]').should('be.visible')
-    })
+  it("Opening", () => {
+    cy.get("[data-testid=modal]").should("be.visible");
+  });
 
-    it('has elements', () => {
-        cy.get('[data-testid=ingredientHeader]').should('exist')
-        cy.get('[data-testid=ingredientData]').should('exist')
-    })
+  it("Elements", () => {
+    cy.get("[data-testid=ingredientHeader]").should("exist");
+    cy.get("[data-testid=ingredientData]").should("exist");
+  });
 
-    it('close button should work', () => {
-        cy.get('[data-testid=close-modal]').should('exist').click()
-        cy.get('[data-testid=modal]').should('not.exist')
-    })
-})
+  it("Close button", () => {
+    cy.get("[data-testid=close-modal]").should("exist").click();
+    cy.get("[data-testid=modal]").should("not.exist");
+  });
+});
 
-describe('drag works', () => {
-    beforeEach(() => {
-        cy.visit('/')
-        cy.get('[data-testid=ingredients-list').as('ingredientList')
-    })
-//
-    it('should move ingredients from ingredient list to basket', () => {
-        const dataTransfer = new DataTransfer()
+describe("Drag ingredients", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("[data-testid=ingredients-list").as("ingredientList");
+  });
+  //
+  it("Move to bucket", () => {
+    const dataTransfer = new DataTransfer();
 
-        cy.get('@ingredientList').eq(1).as('testIngredient');
-        cy.get("@testIngredient").eq(0).children('a').eq(0).as('testInner');
-        cy.get('@testInner').trigger('dragstart', {dataTransfer})
-        cy.get('[data-testid=burgerConstructor]').trigger('drop', {dataTransfer})
+    cy.get("@ingredientList").eq(1).as("testIngredient");
+    cy.get("@testIngredient").eq(0).children("a").eq(0).as("testInner");
+    cy.get("@testInner").trigger("dragstart", { dataTransfer });
+    cy.get("[data-testid=burgerConstructor]").trigger("drop", { dataTransfer });
 
-        cy.get('[data-testid=constructorElement]').should('exist')
-    })
-})
+    cy.get("[data-testid=constructorElement]").should("exist");
+  });
+});
 
-describe('order works', () => {
-    beforeEach(() => {
-        cy.visit('/login')
+describe("Order", () => {
+  beforeEach(() => {
+    cy.visit("/login");
 
-        cy.intercept('GET', INGREDIENTS_URL, {fixture: 'ingredients.json'})
+    cy.intercept("GET", INGREDIENTS_URL, { fixture: "ingredients.json" });
 
-        cy.intercept('POST', LOGIN_URL, {
-            fixture: 'user.json'
-        }).as('postLogin')
+    cy.intercept("POST", LOGIN_URL, {
+      fixture: "user.json",
+    }).as("postLogin");
 
+    cy.get("[data-testid=email-input").type(`test@test.com`);
+    cy.get("[type=password]").type(`testtesttest`);
 
-        cy.get('[data-testid=email-input').type(`test@test.com`)
-        cy.get('[type=password]').type(`testtesttest`)
+    cy.get("[type=submit]").click();
 
-        cy.get('[type=submit]').click()
+    cy.wait("@postLogin");
 
-        cy.wait("@postLogin")
+    cy.get("[data-testid=ingredients-list").as("ingredientList");
 
-        cy.get('[data-testid=ingredients-list').as('ingredientList')
+    const dataTransfer = new DataTransfer();
 
-        const dataTransfer = new DataTransfer()
+    cy.get("@ingredientList").eq(0).as("testIngredient");
+    cy.get("@testIngredient").eq(0).children("a").eq(0).as("testInner");
+    cy.get("@testInner").trigger("dragstart", { dataTransfer });
+    cy.get("[data-testid=burgerConstructor]").trigger("drop", { dataTransfer });
 
-        cy.get('@ingredientList').eq(0).as('testIngredient');
-        cy.get("@testIngredient").eq(0).children('a').eq(0).as('testInner');
-        cy.get('@testInner').trigger('dragstart', {dataTransfer})
-        cy.get('[data-testid=burgerConstructor]').trigger('drop', {dataTransfer})
+    cy.intercept("POST", ORDER_URL, { fixture: "order.json" });
+    cy.get("[data-testid=orderButton]").click();
+  });
 
-        cy.intercept('POST', ORDER_URL, {fixture: 'order.json'})
-        cy.get('[data-testid=orderButton]').click()
-    })
+  it("Show modal", () => {
+    cy.get("[data-testid=modal]").should("be.visible");
+  });
 
-    it('should show order modal', () => {
-        cy.get('[data-testid=modal]').should('be.visible');
-    })
+  it("Order number", () => {
+    cy.get("[data-testid=orderNumber").should("have.text", "42");
+  });
 
-    it('should have order number', () => {
-        cy.get('[data-testid=orderNumber').should('have.text', '42')
-    })
-
-    it('should close', () => {
-        cy.get('[data-testid=close-modal]').should('exist').click()
-        cy.get('[data-testid=modal]').should('not.exist')
-    })
-})
+  it("Close", () => {
+    cy.get("[data-testid=close-modal]").should("exist").click();
+    cy.get("[data-testid=modal]").should("not.exist");
+  });
+});
