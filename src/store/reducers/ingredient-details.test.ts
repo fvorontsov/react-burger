665e7f631f71b25bc5f777dfbfa44782758bd271
fvorontsov <vorontsovfa@gmail.com>
2023-07-26 21:30:20 +0300

    ingredient details tests

--- src/store/reducers/ingredient-details.test.ts
+++ src/store/reducers/ingredient-details.test.ts
@@ -1,4 +1,10 @@
 import { ingredientDetailsSlice } from "./IngredientDetailsSlice";
+import { TCountedIngredient } from "../../types";
+import {
+  closeIngredientDetailsModal,
+  deselectIngredient, openIngredientDetailsModal,
+  selectIngredient,
+} from "../actions/IngredientDetailsActions";
 
 describe("Ingredient Details reducers", () => {
   const reducer = ingredientDetailsSlice.reducer;
@@ -7,5 +13,73 @@ describe("Ingredient Details reducers", () => {
       selectedIngredient: undefined,
       modalIsOpen: false,
     });
+  });
+
+  it("should select ingredient", function () {
+    const ingredient: TCountedIngredient = {
+      quantity: 10,
+      _id: "1",
+      calories: 0,
+      carbohydrates: 0,
+      fat: 0,
+      image: "",
+      image_large: "",
+      image_mobile: "",
+      name: "",
+      price: 0,
+      proteins: 0,
+      type: "bun",
+    };
+
+    expect(reducer(undefined, selectIngredient(ingredient))).toEqual({
+      selectedIngredient: ingredient,
+      modalIsOpen: false,
+    });
+  });
+
+  it("should deselect ingredients", function () {
+    const ingredient: TCountedIngredient = {
+      quantity: 10,
+      _id: "1",
+      calories: 0,
+      carbohydrates: 0,
+      fat: 0,
+      image: "",
+      image_large: "",
+      image_mobile: "",
+      name: "",
+      price: 0,
+      proteins: 0,
+      type: "bun",
+    };
+
+    const testState = {
+      selectedIngredient: ingredient,
+      modalIsOpen: false,
+    };
+
+    expect(reducer(testState, deselectIngredient)).toEqual({
+      selectedIngredient: undefined,
+      modalIsOpen: false,
+    });
+  });
+
+  it("should open modal", function () {
+    expect(reducer(undefined, openIngredientDetailsModal)).toEqual({
+      selectedIngredient: undefined,
+      modalIsOpen: true,
+    })
+  });
+
+  it("should close modal", function () {
+    const testState = {
+      selectedIngredient: undefined,
+      modalIsOpen: true,
+    }
+
+    expect(reducer(testState, closeIngredientDetailsModal)).toEqual({
+      selectedIngredient: undefined,
+      modalIsOpen: false,
+    })
   });
 });
