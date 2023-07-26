f761b21f0a823fd064920bfbad4c14ea8c8a62d6
fvorontsov <vorontsovfa@gmail.com>
2023-07-26 21:44:25 +0300

    user tests

--- src/store/reducers/user.test.ts
+++ src/store/reducers/user.test.ts
@@ -1,9 +1,88 @@
 import { userSlice } from "./UserSlice";
+import {
+  clear,
+  setIsAuthChecked,
+  setUser,
+  userLogin,
+  userRegister,
+} from "../actions/UserActions";
 
 describe("User reducers", () => {
   const reducer = userSlice.reducer;
+
+  const initialState = {
+    user: null,
+    isAuthChecked: false,
+  };
+
   it("has initial state", () => {
-    expect(reducer(undefined, { type: "test" })).toEqual({
+    expect(reducer(undefined, { type: "test" })).toEqual(initialState);
+  });
+
+  it("should set user", function () {
+    const user = {
+      name: "test",
+      email: "test",
+    };
+    expect(reducer(initialState, setUser(user))).toEqual({
+      user: user,
+      isAuthChecked: true,
+    });
+  });
+
+  it("userRegister.fulfilled", () => {
+    const user = {
+      name: "test",
+      email: "test",
+    };
+
+    expect(
+      reducer(
+        initialState,
+        userRegister.fulfilled(user, "", { ...user, password: "x" })
+      )
+    ).toEqual({
+      user: user,
+      isAuthChecked: true,
+    });
+  });
+
+  it("userLogin.fulfilled", () => {
+    const user = {
+      name: "test",
+      email: "test",
+    };
+
+    expect(
+      reducer(
+        initialState,
+        userLogin.fulfilled(user, "", { ...user, password: "x" })
+      )
+    ).toEqual({
+      user: user,
+      isAuthChecked: true,
+    });
+  });
+
+  it("should set auth checked", function () {
+    expect(reducer(initialState, setIsAuthChecked(true))).toEqual({
+      user: null,
+      isAuthChecked: true,
+    });
+  });
+
+  it("should clear", function () {
+    const user = {
+      name: "test",
+      email: "test",
+    };
+
+    const testState = {
+      user: user,
+      isAuthChecked: false,
+    };
+
+    expect(reducer(testState, clear)).toEqual({
       user: null,
       isAuthChecked: false,
     });
